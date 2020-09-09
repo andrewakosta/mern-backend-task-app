2c89bff42fc001f1f20e182ca47b2a04d1539139
Andres Acosta <andresakosta@outlook.com>
2020-09-08 21:16:37 -0500

    Express validator added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,13 @@
 const User = require('../models/User')
+const {validationResult} = require('express-validator')
 
 
 exports.createUser =  async (req,res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
 
     const {email, password} = req.body
 
--- router/users.js
+++ router/users.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const {check} =require('express-validator')
 
 router.post('/',
+    [
+        check('name','The name is mandatory').not().isEmpty(),
+        check('email','Add an Email correct').isEmail(),
+        check('password','The password must have more than 6 characters').isLength({min:6})
+    ],
     userController.createUser
 )
 
