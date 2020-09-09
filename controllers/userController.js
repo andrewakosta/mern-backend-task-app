e0b5b230d5f9804930db2e970cd2cab84db9a5cc
Andres Acosta <andresakosta@outlook.com>
2020-09-08 20:30:13 -0500

    User insert ssuccesful

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
-exports.createUser = (req,res) => {
-    console.log(req.body)
-    res.body = 'puto el que lo lea'
-    return res
+const User = require('../models/User')
+
+
+exports.createUser =  async (req,res) => {
+    try {
+        let user
+        //create a new user
+        user = new User(req.body)
+
+        //Save user
+        await user.save()
+
+        res.status(200).send('The user has been created ssuccesful')
+    } catch (error) {
+        console.log('An erro has ocurred')
+        res.status(400).send('An erro has ocurred')
+    }
 }
