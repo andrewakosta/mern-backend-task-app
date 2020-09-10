505aa3e0354021273c3329be385e7060ff587492
Andres Acosta <andresakosta@outlook.com>
2020-09-10 18:18:12 -0500

    Controller projects updated

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,3 +1,12 @@
+const Project = require("../models/Project")
+
 exports.createProject = async (req, res) => {
-    
+    try {
+        const project = new Project(req.body)
+        await project.save()
+        res.json(project)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('An error has courred')
+    }
 }
--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const PORT =process.env.PORT || 4000
 //Import paths
 app.use('/api/users', require('./router/users'))
 app.use('/api/auth', require('./router/auth'))
-app.use('api/projects',require('./router/projects'))
+app.use('/api/projects',require('./router/projects'))
 //start app 
 app.listen(PORT, () => {
-    console.log('----------------------The server is runing on the port 4000---------------------')
+    console.log('----------------------The server is runing on the port 5000---------------------')
 })
