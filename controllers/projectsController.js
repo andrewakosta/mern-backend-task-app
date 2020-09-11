9b01fecdb53be81e04e51a584808012cc88caa3a
Andres Acosta <andresakosta@outlook.com>
2020-09-10 20:42:06 -0500

    Express validation

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,6 +1,15 @@
 const Project = require("../models/Project")
+const {validationResult} = require('express-validator')
+
 
 exports.createProject = async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        res.status(400).json({errors: errors.array()})
+    }
+
     try {
         const project = new Project(req.body)
 
@@ -9,6 +18,7 @@ exports.createProject = async (req, res) => {
         //Save Project 
         await project.save()
         res.json(project)
+
     } catch (error) {
         console.log(error)
         res.status(500).send('An error has courred')
--- router/projects.js
+++ router/projects.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 const projectsController = require('../controllers/projectsController')
 const auth = require('../middleware/auth')
+const {check} = require('express-validator')
 
 //Create a Project 
 router.post('/',
+    [
+        check('name', "Project's name is madatory").not().isEmpty()
+    ],
     auth,
     projectsController.createProject
 )
